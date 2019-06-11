88399eaed58792e5f0caf41b7f7cc8c7097ff10d
Leo Zhou <glasslion@gmail.com>
2019-06-11 17:58:01 +0800

    models

---
+++ models.py
@@ -0,0 +1,27 @@
+from peewee import *
+import datetime
+
+import os
+
+
+db = SqliteDatabase('my_database.db')
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class Book(BaseModel):
+    url = CharField()
+    name = CharField()
+    author = CharField()
+    category = CharField()
+    length = IntegerField()
+    rating_count = IntegerField()
+    rating_avg = FloatField()
+    update_date = CharField()
+
+
+if __name__ == '__main__':
+    db.connect()
+    db.create_tables([Book], safe=True)
